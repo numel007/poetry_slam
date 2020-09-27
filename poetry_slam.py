f018fa02232de47965d8ddd69b6501fdd0f84ee1
Jackie <taekwondo.chan@gmail.com>
2020-09-27 15:41:21 -0700

    add lines_printed_backwards

--- poetry_slam.py
+++ poetry_slam.py
@@ -4,18 +4,16 @@ poem_lines = []
 def get_file_lines(filename):
     line_count = 0
     for line in filename:
-        line = line.split(":")
-        print(line)
+        line = line.rstrip("\n").split(":")
         poem_lines.append(line)
         line_count += 1
-    print(f"{line_count} lines")
 
     return poem_lines
 
+
+def lines_printed_backwards(filename):
+    for line_num, text in reversed(poem_lines):
+        print(line_num, text)
+
 get_file_lines(infile)
-
-# def lines_printed_backwards(filename):
-    
-
-
-# lines_printed_backwards(infile)
+lines_printed_backwards(infile)
