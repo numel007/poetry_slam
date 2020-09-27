3e7c01621639a5d8e4fc56d7cf14c44e325db5e2
Jackie <taekwondo.chan@gmail.com>
2020-09-27 15:56:46 -0700

    add random function

--- poetry_slam.py
+++ poetry_slam.py
@@ -1,3 +1,4 @@
+from random import choice
 infile = open("poem.txt", "r")
 lines_list = []
 
@@ -8,13 +9,28 @@ def get_file_lines(filename):
         lines_list.append(line)
         line_count += 1
 
+    print("\n")
+
     return lines_list
 
 
 def lines_printed_backwards(lines_list):
     for line_num, text in reversed(lines_list):
         print(line_num, text)
+    print("\n")
+
+def lines_printed_random(lines_list):
+    randPoem = []
+    poem_length = len(lines_list)
+    for i in range(poem_length):
+        randPoem.append(choice(lines_list))
+        i += 1
+    
+    for line_num, text in randPoem:
+        print(line_num, text)
+    print("\n")
 
 
 get_file_lines(infile)
 lines_printed_backwards(lines_list)
+lines_printed_random(lines_list)
