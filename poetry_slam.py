909e4893c1c6ac43d559a4c4f8fb3f3f222c2ad3
Jackie <taekwondo.chan@gmail.com>
2020-09-27 15:28:38 -0700

    add get_file_lines

--- poetry_slam.py
+++ poetry_slam.py
@@ -1,2 +1,21 @@
+infile = open("poem.txt", "r")
+poem_lines = []
+
 def get_file_lines(filename):
+    line_count = 0
+    for line in filename:
+        line = line.split(":")
+        print(line)
+        poem_lines.append(line)
+        line_count += 1
+    print(f"{line_count} lines")
+
+    return poem_lines
+
+get_file_lines(infile)
+
+# def lines_printed_backwards(filename):
     
+
+
+# lines_printed_backwards(infile)
