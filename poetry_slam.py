bf5e7d68f9610dd6473f13321b649ad2473f4eed
Jackie <taekwondo.chan@gmail.com>
2020-09-30 19:11:06 -0700

    add custom function

--- poetry_slam.py
+++ poetry_slam.py
@@ -1,36 +1,66 @@
+# Import random module and choice method from random module
 from random import choice
+import random
+
+# Open poem.txt and store in infile
 infile = open("poem.txt", "r")
-lines_list = []
+
+# Initialize an empty dictionary
+lines_list = {}
 
 def get_file_lines(filename):
-    line_count = 0
-    for line in filename:
-        line = line.rstrip("\n").split(":")
-        lines_list.append(line)
-        line_count += 1
 
-    print("\n")
-
+    # Iterate through poem.txt
+    for line in infile:
+        # Split each line at ":" and remove line breaks. Store in a list called split_poem
+        split_poem = line.rstrip().split(": ")
+        # Store index 0 of split_poem in line_num for future reference
+        line_num = split_poem[0]
+        # Store index 1 of split_poem in poem_text for future reference
+        poem_text = split_poem[1]
+        # Add keys and values of line_num and poem_text into dictionary, respectively 
+        lines_list[line_num] = poem_text
+    
     return lines_list
 
 
 def lines_printed_backwards(lines_list):
-    for line_num, text in reversed(lines_list):
+    # Iterate through lines_list and print each line backwards, using the reversed method
+    for line_num, text in reversed(lines_list.items()):
         print(line_num, text)
+
     print("\n")
 
 def lines_printed_random(lines_list):
-    randPoem = []
+    # Initialize new dictionary
+    randPoem = {}
+    # Get number of lines in poem
     poem_length = len(lines_list)
+
     for i in range(poem_length):
-        randPoem.append(choice(lines_list))
+        # Convert lines_list into a list and scramble with choice. Store in randPoem dictionary.
+        randPoem[i] = choice(list(lines_list.items()))
         i += 1
-    
-    for line_num, text in randPoem:
-        print(line_num, text)
-    print("\n")
 
+    # Print just the values stored in randPoem
+    for line_num, text in randPoem.items():
+        print(text)
+    print('\n')
 
+def lines_printed_custom(lines_list):
+    # Convert lines_list into list and store in lines_list_custom
+    lines_list_custom = list(lines_list.items())
+    # Scramble lines_list_custom using shuffle method
+    random.shuffle(lines_list_custom)
+    # Convert lines_list_custom into dictionary and store in lines_list
+    lines_list = dict(lines_list_custom)
+
+    # Print values of lines_list dictionary
+    for i in lines_list:
+        print(lines_list[i])
+
+# Call functions
 get_file_lines(infile)
 lines_printed_backwards(lines_list)
 lines_printed_random(lines_list)
+lines_printed_custom(lines_list)
